aaeb89098817dc981f85500a5dcdde1a6422a6a9
yaohong <yaohong6782@gmail.com>
2022-03-23 21:51:05 +0800

    header and about components added

--- src/components/Navigations/Navigation.js
+++ src/components/Navigations/Navigation.js
@@ -5,24 +5,73 @@ import { FaTasks, FaUserAlt, FaUserGraduate, FaBook } from "react-icons/fa";
 import { BsBriefcaseFill } from "react-icons/bs";
 
 const Navigation = () => {
+  const [currentNav, setCurrentNav] = useState("#home");
+  const hightLightActiveNav = (props) => {
+    switch (props) {
+      case "#home":
+        setCurrentNav("#home");
+        break;
+      case "#about":
+        setCurrentNav("#about");
+        break;
+      case "#education":
+        setCurrentNav("#education");
+        break;
+      case "#projects":
+        setCurrentNav("#projects");
+        break;
+      case "#experience":
+        setCurrentNav("#experience");
+        break;
+      case "#tech":
+        setCurrentNav("#tech");
+        break;
+      default:
+        setCurrentNav("#home");
+    }
+  };
   return (
     <nav>
-      <a href="#home" className="active">
+      <a
+        href="#home"
+        onClick={() => hightLightActiveNav("#home")}
+        className={currentNav === "#home" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => hightLightActiveNav("#about")}
+        className={currentNav === "#about" ? "active" : ""}
+      >
         <FaUserAlt />
       </a>
-      <a href="#education">
+      <a
+        href="#education"
+        onClick={() => hightLightActiveNav("#education")}
+        className={currentNav === "#education" ? "active" : ""}
+      >
         <FaUserGraduate />
       </a>
-      <a href="#projects">
+      <a
+        href="#projects"
+        onClick={() => hightLightActiveNav("#projects")}
+        className={currentNav === "#projects" ? "active" : ""}
+      >
         <FaTasks />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => hightLightActiveNav("#experience")}
+        className={currentNav === "#experience" ? "active" : ""}
+      >
         <BsBriefcaseFill />
       </a>
-      <a href="#tech">
+      <a
+        href="#tech"
+        onClick={() => hightLightActiveNav("#tech")}
+        className={currentNav === "#tech" ? "active" : ""}
+      >
         <FaBook />
       </a>
     </nav>
--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { GiStairsGoal, GiSleepingBag, GiVacuumCleaner } from "react-icons/gi";
+import { IoGameControllerOutline } from "react-icons/io5";
+
+import "./About.css";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h2>More About Me</h2>
+
+      <div className="container about__container">
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <GiStairsGoal className="about__icon" />
+              <h2>Goals</h2>
+              <medium>On a Job hunt for Entry Level Position</medium>
+            </article>
+
+            <article className="about__card">
+              <IoGameControllerOutline className="about__icon" />
+              <h2>Hobbies</h2>
+              <medium>Video Games and Algorithms</medium>
+            </article>
+
+            <article className="about__card">
+              <GiVacuumCleaner className="about__icon" />
+              <h2>Miscellaneous</h2>
+              <medium>I like to analyse and clean up codes</medium>
+            </article>
+          </div>
+          <p>ASDF</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
