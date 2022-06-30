a94638962b5aa6e964c4e7f64fb621ab0ef6cd71
yaohong <yaohong6782@gmail.com>
2022-07-01 00:53:38 +0800

    added new timeline component design

--- src/components/body/education/Education.js
+++ src/components/body/education/Education.js
@@ -13,7 +13,7 @@ const Education = () => {
       id: 1,
       school: "University of Wollonggong",
       where: "",
-      course: "Computer Science (Cybersecurity)",
+      course: "Computer Science - Cybersecurity",
       duration: "2020 - 2022",
     },
     {
@@ -30,53 +30,39 @@ const Education = () => {
       <h2>Education</h2>
 
       <div className="container education__container">
-        {EducationList.map((item, id) => {
-          return (
-            <div className="education-wrapper" key={id}>
-              <div className="edu__wrapper">
-                <h3 className="edu-school">{item.school} </h3>
-                <div className="edu__course-year">
-                  <h3 className="edu-course">{item.course}</h3>
-                  <small className="edu-year">{item.duration}</small>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+        <VerticalTimeline>
+          {EducationList.map((item, id) => {
+            return (
+              <VerticalTimelineElement key={id}
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  background: "rgb(33, 150, 243)",
+                  color: "#fff",
+                }}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(33, 150, 243)",
+                }}
+                date={item.duration}
+                iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {item.school}
+                </h3>
+                <p>{item.course}</p>
+                {/* <div className="education-wrapper" key={id}>
+                  <div className="edu__wrapper">
+                    <h3 className="edu-school">{item.school} </h3>
+                    <div className="edu__course-year">
+                      <h3 className="edu-course">{item.course}</h3>
+                      <small className="edu-year">{item.duration}</small>
+                    </div>
+                  </div>
+                </div> */}
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
       </div>
-
-      {/* figure out how to implement into a map */}
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-
-      </VerticalTimeline>
     </section>
   );
 };
